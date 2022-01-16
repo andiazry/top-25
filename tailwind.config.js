83cffc3662bb83b56fbde284f4819b0c54efdc2f
andiazry <andiazry@gmail.com>
2022-01-16 18:45:51 -0500

    type styles test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'neue': ['neue', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
