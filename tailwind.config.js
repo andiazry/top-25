ca9817f67b96804167778248e588552265839642
andiazry <andiazry@gmail.com>
2022-01-18 20:10:27 -0500

    fixed scrolling in divs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    colors: {
-      customYellow: '#FFEF93',
-    },
     extend: {
       fontFamily: {
         'neue': ['neue', 'sans-serif'],
         'rounded': ['pangram-rounded', 'neue'],
         'accent': ['object-sans', 'neue']
-      }
+      },
+      colors: {
+        customYellow: '#FFEF93',
+      },
     },
   },
   plugins: [],
