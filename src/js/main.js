e1dba5029d3b07fa53ea974d85459ce58b383684
andiazry <andiazry@gmail.com>
2022-01-18 19:40:08 -0500

    initial layout for top 25 template

---
+++ src/js/main.js
@@ -0,0 +1,2 @@
+var rotateValues = [-3, 3]
+var rotateValue = rotateValues[Math.floor(Math.random()*rotateValues.length)];
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      customYellow: '#FFEF93',
+    },
     extend: {
       fontFamily: {
-        'neue': ['neue', 'sans-serif']
+        'neue': ['neue', 'sans-serif'],
+        'rounded': ['pangram-rounded', 'neue'],
+        'accent': ['object-sans', 'neue']
       }
     },
   },
